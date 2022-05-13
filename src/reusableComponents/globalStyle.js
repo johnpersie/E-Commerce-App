8a0280f3577433b407fa9272a8a55d382fdd2382
johnpersie <83553199+johnpersie@users.noreply.github.com>
2022-05-13 14:59:57 +0100

    intial commit

---
+++ src/reusableComponents/globalStyle.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+*,
+*::before,
+*::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+html {
+    font-size: 62.5%;
+}
+
+a{
+    text-decoration: none;
+}
+
+ul{
+    list-style-type: none;
+    
+}
+
+body {
+    font-size: 1.6rem;
+    font-family: 'Urbanist', sans-serif;
+   
+}
+
+`;
