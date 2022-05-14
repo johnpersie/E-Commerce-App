75f84d5a2826bbcc3f16b19a54d64629a3534e71
johnpersie <83553199+johnpersie@users.noreply.github.com>
2022-05-14 11:11:18 +0100

    create the navigation bar

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -1,7 +1,32 @@
+import { Search } from "@mui/icons-material";
 import React from "react";
+import {
+  Center,
+  Container,
+  Input,
+  Language,
+  Left,
+  Right,
+  SearchContainer,
+  Wrapper,
+} from "./navBarStyled";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <Search />
+          </SearchContainer>
+        </Left>
+        <Center>Center</Center>
+        <Right>Right</Right>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default NavBar;
---
+++ src/components/navBar/navBarStyled.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 6rem;
+`;
+export const Wrapper = styled.div`
+  padding: 1rem 2rem;
+  display: flex;
+  justify-content: space-between;
+`;
+export const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+export const Language = styled.span`
+  font-size: 1.4rem;
+  cursor: pointer;
+`;
+export const SearchContainer = styled.div`
+  border: 0.5px solid gray;
+  display: flex;
+  align-items: center;
+  margin-left: 2.5rem;
+  padding: 0.5rem;
+`;
+export const Input = styled.input`
+  border: none;
+`;
+export const Center = styled.div`
+  flex: 1;
+`;
+export const Right = styled.div`
+  flex: 1;
+`;
